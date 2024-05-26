9eba6a631148a8cf2e78c4932667a0a1cf72f661
Anastasia Lomova <lomovaanastasia@redcollar.ru>
2024-05-26 17:07:02 +0300

    feat: add useCount hook

--- src/components/dish/component.tsx
+++ src/components/dish/component.tsx
@@ -1,10 +1,22 @@
 import { FC } from "react";
 import { RestaurantDish } from "../../types";
-import { Counter } from "../counter/component";
+import { useCount } from "../../hooks/use-count";
 
-export const Dish: FC<RestaurantDish> = ({ name }) => (
-  <div>
-    <p>{name}</p>
-    <Counter />
-  </div>
-);
+export const Dish: FC<RestaurantDish> = ({ name }) => {
+  const { counter, increment, decrement } = useCount();
+
+  return (
+    <div>
+      <p>{name}</p>
+      <div>
+        <p>{counter}</p>
+        <button type="button" onClick={increment}>
+          +
+        </button>
+        <button type="button" onClick={decrement}>
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
